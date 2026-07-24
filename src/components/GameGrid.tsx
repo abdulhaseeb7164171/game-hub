5acf90b038ac7429c1afbdf80046521bfdaaa5f0
Abdul Haseeb <abdulhaseeb7164171@gmail.com>
2026-07-24 12:25:23 +0500

    Enhance GameGrid with infinite scrolling and loading state; update useGames for pagination support

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,5 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text, Button, Box, Spinner } from "@chakra-ui/react";
+import React from "react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -10,28 +11,53 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const { 
+    data, 
+    error, 
+    isLoading, 
+    isFetchingNextPage, 
+    fetchNextPage, 
+    hasNextPage 
+  } = useGames(gameQuery);
 
   if (error) return <Text>{error.message}</Text>;
 
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-      spacing={6}
-      padding="10px"
-    >
-      {isLoading &&
-        [...Array(6)].map((_, index) => (
-          <GameCardContainer key={index}>
-            <GameCardSkeleton />
-          </GameCardContainer>
+    <Box padding="10px">
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={6}
+        marginBottom={5}
+      >
+        {isLoading &&
+          [...Array(6)].map((_, index) => (
+            <GameCardContainer key={index}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))}
+          </React.Fragment>
         ))}
-      {data?.results.map((game) => (
-        <GameCardContainer key={game.id}>
-          <GameCard game={game} />
-        </GameCardContainer>
-      ))}
-    </SimpleGrid>
+      </SimpleGrid>
+      
+      {hasNextPage && (
+        <Box display="flex" justifyContent="center">
+          <Button 
+            onClick={() => fetchNextPage()} 
+            disabled={isFetchingNextPage}
+            marginY={5}
+          >
+            {isFetchingNextPage ? <Spinner /> : "Load More"}
+          </Button>
+        </Box>
+      )}
+    </Box>
   );
 };
 
