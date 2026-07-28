af5b9ef184eb261adf1ceca8a51cceeacea541b3
Abdul Haseeb <abdulhaseeb7164171@gmail.com>
2026-07-28 12:42:04 +0500

    feat: wrap logo in Link component for navigation and enhance search input submission handling

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,21 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import { gameQueryActions } from "../store.actions";
+import { useNavigate } from "@tanstack/react-router";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
 
-        if (ref.current) gameQueryActions.setSearchText(ref.current.value);
+        if (ref.current) {
+          gameQueryActions.setSearchText(ref.current.value);
+          navigate({ to: "/" });
+        }
       }}
     >
       <InputGroup>
